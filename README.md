cf1838b323ad8dce4792d05b4e4443b9af30895b
Vladislav <vladislav.marchak@gmail.com>
2021-07-27 00:21:52 +0300

    Дописал, указанные тесты. тест запускается командой npm run test:jest

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+
+<!-- npm run test:jest для запуска теста -->
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- tests/sum.test.js
+++ tests/sum.test.js
@@ -1,5 +1,24 @@
 const runLogic = require("../src/logic.trans");
 
-test("sum test", () => {
+test("test0: qwe_qwe_qwe", () => {
  expect(runLogic("qwe_qwe_qwe")).toBe("qweQweQwe");
 });
+
+test("test1: test_test", () => {
+ expect(runLogic("test_test")).toBe("testTest");
+});
+
+test("test2: test-_-test", () => {
+ expect(runLogic("test-_-test")).toBe("test--test");
+});
+
+test("test3: test__test", () => {
+ expect(runLogic("test__test")).toBe("testTest");
+});
+
+test("test4: test_t_e_s_t", () => {
+ expect(runLogic("test_t_e_s_t")).toBe("testTEST");
+});
+
+
+
